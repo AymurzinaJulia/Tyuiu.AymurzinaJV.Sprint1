3e12ca800c4792a871eaa9bfbb70f7ce2b64505a
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-09-23 22:36:42 +0500

    Task3 added

---
+++ Tyuiu.AymurzinaJV.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AymurzinaJV.Sprint1.Task3.V7.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 90;
+            var res = ds.VerstsToKilometers(x);
+            Assert.AreEqual(96012, res);
+        }
+    }
+}
+    
---
+++ Tyuiu.AymurzinaJV.Sprint1.Task3.V7/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.AymurzinaJV.Sprint1.Task3.V7.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнила: Аймурзина Юлия Владимировна | РППб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+double x;
+
+Console.WriteLine("Введите расстояние в верстах:");
+x = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.VerstsToKilometers(x));
+
+Console.ReadLine();
