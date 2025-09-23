63bc73487ac389df0efc912ddf3c64637147d1cc
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-09-23 22:07:28 +0500

    Task2 added

---
+++ Tyuiu.AymurzinaJV.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AymurzinaJV.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.AymurzinaJV.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 61;
+            var res = ds.ConvertSecondsToHours(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.AymurzinaJV.Sprint1.Task2.V29.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Организация ввода и вывода в консольных приложениях               *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #29                                                              *");
+Console.WriteLine("* Выполнила: Аймурзина Юлия Владимировна | РППб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+int x;
+
+Console.WriteLine("Введите время в секундах:");
+x = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.ConvertSecondsToHours(x));
+
+Console.ReadLine();
